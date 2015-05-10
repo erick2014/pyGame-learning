dee8d936f5d2a7a1d15c980272285176183eea92
erick2014 <erickarmandogarcia@hotmail.com>
2015-05-10 15:49:28 -0500

    First game is over.

--- game/game1.py
+++ game/game1.py
@@ -8,24 +8,36 @@ tanks = {
         }
 
 aliveTanks=len(tanks)
+secondName=""
+result=""
 
 while aliveTanks > 1:
 
   #validate the victims
   try :
+    print "################################"
     #get the shooter
     first=raw_input("Who fires? ").lower()
     first=tanks[first]
+
     #get the victim
     second=raw_input("Who at? ").lower()
+    secondName=second
     second=tanks[second]
 
   except KeyError,name:
-    print "No such tank!",name
+    print "The enemy doesnt exists or is dead",name
     continue
 
   #shoot
   result=first.fire_at(second)
-  print "result",result
+
+  if ( result == "dead" ):
+    aliveTanks-=1
+    #remove the victim who dies
+    del tanks[secondName]
+
+print "Who wins the fight?",tanks.keys()
+print "Congratz nigga!"
 
 
--- game/tank.py
+++ game/tank.py
@@ -6,7 +6,7 @@ class Tank(object):
     self.alive = True
     self.ammo = 5
     self.armor = 60
-    self.result=""
+    self.deadMan=""
 
   def __str__(self):
     if self.alive:
@@ -15,30 +15,35 @@ class Tank(object):
       return "%s (DEAD)" % self.name
 
   def fire_at(self, enemy):
+    result=""
+    print "################################"
     if self.ammo >= 1:
       self.ammo -= 1
       print self.name, "fires on", enemy.name
-      self.result=enemy.hit()
+      result=enemy.hit()
 
     else:
-      self.result=self.name, "has no shells!"
-      print result
+      result="noAmmo"
+      print self.name, "has no shells!"
 
-    return self.result
+    return result
 
   def hit(self):
+    result=""
 
     self.armor -= 20
-    self.result=self.name+" is hit!"
-    print self.result
+    print self.name+" is hit!"
     print "%s's remaining life is %i" % (self.name,self.armor)
+    result="hit"
 
     if self.armor <= 0:
-      self.result=self.explode()
+      self.explode()
+      result="dead"
 
-    return self.result
+    return result
 
   def explode(self):
     self.alive = False
+    print "xxxxxxxxxxxxxxxxxxxxx"
     print self.name, "explodes!"
-    return "isDead"
+    print "xxxxxxxxxxxxxxxxxxxxx"
